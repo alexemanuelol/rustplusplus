aceff490535b8948b5ef317ed830656e39b18288
Alexander Emanuelsson <alexander.emanuelsson94@gmail.com>
2021-10-20 23:16:50 +0200

    Add new vending machine detected notification

--- inGameEvents/vendingMachine.js
+++ inGameEvents/vendingMachine.js
@@ -1,5 +1,25 @@
+const MapCalc = require('./../utils/mapCalculations.js');
+const RustPlusTypes = require('../utils/rustplusTypes.js');
+
+var currentVendingMachines = [];
+
 module.exports = {
     checkEvent: function (discord, rustplus, info, mapMarkers, teamInfo, time) {
-        // TODO
+        /* Check if new Vending Machine is detected */
+        for (let marker of mapMarkers.response.mapMarkers.markers) {
+            if (marker.type === RustPlusTypes.MarkerType.VendingMachine) {
+                if (!currentVendingMachines.some(e => e.x === marker.x && e.y === marker.y)) {
+                    currentVendingMachines.push({ x: marker.x, y: marker.y });
+
+                    let gridLocation = MapCalc.getGridPos(marker.x, marker.y, info.response.info.mapSize);
+
+                    console.log('New Vending Machine located at ' + gridLocation);
+                }
+            }
+        }
+    },
+
+    clearVendingMachines: function () {
+        currentVendingMachines = [];
     },
 }
