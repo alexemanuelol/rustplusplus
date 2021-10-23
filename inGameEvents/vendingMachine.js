a8e982ee8dd5e66c3694096e05d4be3dde9f5cc8
Alexander Emanuelsson <alexander.emanuelsson94@gmail.com>
2021-10-23 14:17:38 +0200

    Add Vending Machine item found notification for desired items

--- inGameEvents/vendingMachine.js
+++ inGameEvents/vendingMachine.js
@@ -1,25 +1,101 @@
 const MapCalc = require('./../utils/mapCalculations.js');
 const RustPlusTypes = require('../utils/rustplusTypes.js');
+const Items = require('./../utils/items.js');
 
 var currentVendingMachines = [];
+var foundItems = [];
+
+var itemsToLookForId = [];
 
 module.exports = {
     checkEvent: function (discord, rustplus, info, mapMarkers, teamInfo, time) {
-        /* Check if new Vending Machine is detected */
         for (let marker of mapMarkers.response.mapMarkers.markers) {
             if (marker.type === RustPlusTypes.MarkerType.VendingMachine) {
-                if (!currentVendingMachines.some(e => e.x === marker.x && e.y === marker.y)) {
-                    currentVendingMachines.push({ x: marker.x, y: marker.y });
+                /* Check if new Vending Machine is detected */
+                module.exports.checkNewVendingMachineDetected(marker, info);
 
-                    let gridLocation = MapCalc.getGridPos(marker.x, marker.y, info.response.info.mapSize);
+                /* Go through sellOrders to see if it includes items that we are looking for */
+                module.exports.checkItemsFromSellOrders(marker, info);
+            }
+        }
+    },
 
-                    console.log('New Vending Machine located at ' + gridLocation);
+    checkNewVendingMachineDetected: function (marker, info) {
+        if (!currentVendingMachines.some(e => e.x === marker.x && e.y === marker.y)) {
+            currentVendingMachines.push({ x: marker.x, y: marker.y });
+
+            let gridLocation = MapCalc.getGridPos(marker.x, marker.y, info.response.info.mapSize);
+
+            console.log(`New Vending Machine located at ${gridLocation}`);
+        }
+    },
+
+    checkItemsFromSellOrders: function (marker, info) {
+        for (let order of marker.sellOrders) {
+            /* if itemId or currencyId is in itemsToLookForId */
+            if (itemsToLookForId.includes(order.itemId) || itemsToLookForId.includes(order.currencyId)) {
+                if (!module.exports.isAlreadyInFoundItems(marker.x, marker.y, order)) {
+                    if (order.amountInStock >= 1) {
+                        /* Add to the array of found items */
+                        module.exports.addToFoundItems(marker.x, marker.y, order);
+
+                        let item = '';
+                        if (itemsToLookForId.includes(order.itemId) && itemsToLookForId.includes(order.currencyId)) {
+                            item = Items.getName(order.itemId) + ' and ';
+                            item += Items.getName(order.currencyId);
+                        }
+                        else if (itemsToLookForId.includes(order.itemId)) {
+                            item = Items.getName(order.itemId);
+                        }
+                        else if (itemsToLookForId.includes(order.currencyId)) {
+                            item = Items.getName(order.currencyId);
+                        }
+
+                        let gridLocation = MapCalc.getGridPos(marker.x, marker.y, info.response.info.mapSize);
+
+                        console.log(`${item} was found in a Vending Machine at ${gridLocation}`);
+                    }
                 }
             }
         }
     },
 
+    isAlreadyInFoundItems: function (x, y, order) {
+        return foundItems.some(e => e.x === x && e.y === y &&
+            e.itemId === order.itemId && e.quantity === order.quantity &&
+            e.currencyId === order.currencyId && e.costPerItem === order.costPerItem)
+    },
+
+    addToFoundItems: function (x, y, order) {
+        foundItems.push({
+            x: x, y: y, itemId: order.itemId, quantity: order.quantity,
+            currencyId: order.currencyId, costPerItem: order.costPerItem
+        });
+    },
+
     clearVendingMachines: function () {
         currentVendingMachines = [];
+    },
+
+    clearFoundItems: function () {
+        foundItems = [];
+    },
+
+    addItemToLookFor: function (id) {
+        if (!itemsToLookForId.includes(id)) {
+            itemsToLookForId.push(id);
+        }
+    },
+
+    removeItemToLookFor: function (id) {
+        itemsToLookForId = itemsToLookForId.filter(e => e !== id);
+    },
+
+    getItemsToLookFor: function () {
+        return itemsToLookForId;
+    },
+
+    clearItemsToLookFor: function () {
+        itemsToLookForId = [];
     },
 }
--- utils/items.js
+++ utils/items.js
@@ -8,4 +8,55 @@ module.exports = {
     getId: function (name) {
         return Object.keys(Items).find(key => Items[key].name === name);
     },
+
+    getClosestItemIdByName: function (name, similarity = 0.9) {
+        return Object.keys(Items).find(key => module.exports.similarity(Items[key].name, name) >= similarity);
+    },
+
+    similarity: function (str1, str2) {
+        var longer = str1, shorter = str2;
+        if (str1.length < str2.length) {
+            longer = str2;
+            shorter = str1;
+        }
+
+        var longerLength = longer.length;
+        if (longerLength == 0) {
+            return 1.0;
+        }
+
+        return (longerLength - module.exports.editDistance(longer, shorter)) / parseFloat(longerLength);
+    },
+
+    editDistance: function (str1, str2) {
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+
+        var costs = new Array();
+        for (var i = 0; i <= str1.length; i++) {
+            var lastValue = i;
+            for (var j = 0; j <= str2.length; j++) {
+                if (i == 0) {
+                    costs[j] = j;
+                }
+                else {
+                    if (j > 0) {
+                        var newValue = costs[j - 1];
+                        if (str1.charAt(i - 1) != str2.charAt(j - 1)) {
+                            newValue = Math.min(Math.min(newValue, lastValue), costs[j]) + 1;
+                        }
+                        costs[j - 1] = lastValue;
+                        lastValue = newValue;
+                    }
+                }
+            }
+
+            if (i > 0) {
+                costs[str2.length] = lastValue;
+            }
+        }
+
+        return costs[str2.length];
+    },
+
 }
