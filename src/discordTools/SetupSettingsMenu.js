802acd30c58ec9acae5c3eede41146a3957e6905
Alexander Emanuelsson <alexander.emanuelsson94@gmail.com>
2021-12-18 13:26:27 +0100

    Fixed check if button interaction is from notification settings

--- src/discordTools/SetupSettingsMenu.js
+++ src/discordTools/SetupSettingsMenu.js
@@ -1,6 +1,8 @@
 const { MessageEmbed } = require('discord.js');
 const DiscordTools = require('./discordTools.js');
 
+const icon = 'https://upload.wikimedia.org/wikipedia/commons/6/6d/Windows_Settings_app_icon.png';
+
 module.exports = (client, guild) => {
     let channel = DiscordTools.getTextChannelById(guild.id, client.guildsAndChannelsIds[guild.id].settings);
     let instance = client.readInstanceFile(guild.id);
@@ -12,16 +14,8 @@ module.exports = (client, guild) => {
 
     DiscordTools.clearTextChannel(guild.id, client.guildsAndChannelsIds[guild.id].settings, 100);
 
-
     /* GENERAL SETTINGS */
-    channel.send({
-        embeds: [
-            new MessageEmbed()
-                .setColor('#ce412b')
-                .setAuthor('GENERAL SETTINGS',
-                    'https://upload.wikimedia.org/wikipedia/commons/6/6d/Windows_Settings_app_icon.png')
-        ],
-    });
+    channel.send({ embeds: [ new MessageEmbed().setColor('#ce412b').setAuthor('GENERAL SETTINGS', icon) ] });
 
     const embed = new MessageEmbed()
         .setColor('#861c0c')
@@ -30,16 +24,8 @@ module.exports = (client, guild) => {
     const row = DiscordTools.getPrefixSelectMenu(instance.generalSettings.prefix);
     channel.send({ embeds: [embed], components: [row] });
 
-
     /* NOTIFICATION SETTINGS */
-    channel.send({
-        embeds: [
-            new MessageEmbed()
-                .setColor('#ce412b')
-                .setAuthor('NOTIFICATION SETTINGS',
-                    'https://upload.wikimedia.org/wikipedia/commons/6/6d/Windows_Settings_app_icon.png')
-        ],
-    });
+    channel.send({ embeds: [ new MessageEmbed().setColor('#ce412b').setAuthor('NOTIFICATION SETTINGS', icon) ] });
 
     for (let setting in instance.notificationSettings) {
         let discord = instance.notificationSettings[setting].discord;
@@ -49,11 +35,10 @@ module.exports = (client, guild) => {
             .setColor('#861c0c')
             .setDescription(instance.notificationSettings[setting].description);
         let row = DiscordTools.getNotificationButtonsRow(
-            `${setting}Discord`,
+            `${setting}DiscordNotification`,
             discord,
-            `${setting}InGame`,
+            `${setting}InGameNotification`,
             inGame);
         channel.send({ embeds: [embed], components: [row] });
     }
-
 };
--- src/handlers/buttonHandler.js
+++ src/handlers/buttonHandler.js
@@ -3,33 +3,40 @@ const DiscordTools = require('../discordTools/discordTools.js');
 module.exports = (client, interaction) => {
     let guildId = interaction.guildId;
 
-    let setting = interaction.customId.replace('Discord', '').replace('InGame', '');
+    /* Button from notification settings */
+    if (interaction.customId.endsWith('DiscordNotification') ||
+        interaction.customId.endsWith('InGameNotification')) {
+        let setting = interaction.customId.replace('DiscordNotification', '');
+        setting = setting.replace('InGameNotification', '');
 
-    let instance = client.readInstanceFile(guildId);
-    if (interaction.customId.endsWith('Discord')) {
-        instance.notificationSettings[setting].discord = !instance.notificationSettings[setting].discord;
-    }
-    else {
-        instance.notificationSettings[setting].inGame = !instance.notificationSettings[setting].inGame;
-    }
-    client.writeInstanceFile(guildId, instance);
-
-    if (client.rustplusInstances.hasOwnProperty(guildId)) {
-        if (interaction.customId.endsWith('Discord')) {
-            client.rustplusInstances[guildId].notificationSettings[setting].discord =
-                instance.notificationSettings[setting].discord;
+        /* Update instance file */
+        let instance = client.readInstanceFile(guildId);
+        if (interaction.customId.endsWith('DiscordNotification')) {
+            instance.notificationSettings[setting].discord = !instance.notificationSettings[setting].discord;
         }
         else {
-            client.rustplusInstances[guildId].notificationSettings[setting].inGame =
-                instance.notificationSettings[setting].inGame;
+            instance.notificationSettings[setting].inGame = !instance.notificationSettings[setting].inGame;
         }
+        client.writeInstanceFile(guildId, instance);
+
+        /* Update rustplusInstances object */
+        if (client.rustplusInstances.hasOwnProperty(guildId)) {
+            if (interaction.customId.endsWith('DiscordNotification')) {
+                client.rustplusInstances[guildId].notificationSettings[setting].discord =
+                    instance.notificationSettings[setting].discord;
+            }
+            else {
+                client.rustplusInstances[guildId].notificationSettings[setting].inGame =
+                    instance.notificationSettings[setting].inGame;
+            }
+        }
+
+        let row = DiscordTools.getNotificationButtonsRow(
+            `${setting}DiscordNotification`,
+            instance.notificationSettings[setting].discord,
+            `${setting}InGameNotification`,
+            instance.notificationSettings[setting].inGame);
+
+        interaction.update({ components: [row] });
     }
-
-    let row = DiscordTools.getNotificationButtonsRow(
-        `${setting}Discord`,
-        instance.notificationSettings[setting].discord,
-        `${setting}InGame`,
-        instance.notificationSettings[setting].inGame);
-
-    interaction.update({ components: [row] });
 }
