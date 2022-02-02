d331533aed797ec4ea0af5b26853864b9e7ed325
Alexander Emanuelsson <alexander.emanuelsson94@gmail.com>
2022-02-02 21:31:19 +0100

    Fix sometimes it crash because of error 'user aboerted request'

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ if (!fs.existsSync(`${__dirname}/src/logs`)) {
     fs.mkdirSync(`${__dirname}/src/logs`);
 }
 
-const client = new DiscordBot({ intents: [Discord.Intents.FLAGS.GUILDS] });
+const client = new DiscordBot({ intents: [Discord.Intents.FLAGS.GUILDS], retryLimit: 2 });
 client.build();
 
 exports.client = client;
