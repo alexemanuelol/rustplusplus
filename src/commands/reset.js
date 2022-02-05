92f7fca4bdd3a9086d3390acfcb415e5dcd27c04
Alexander Emanuelsson <alexander.emanuelsson94@gmail.com>
2022-02-06 00:31:21 +0100

    Add property to check if settings need to be resent

--- src/commands/reset.js
+++ src/commands/reset.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const DiscordTools = require('../discordTools/discordTools.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -13,11 +14,14 @@ module.exports = {
 
 		switch (interaction.options.getSubcommand()) {
 			case 'discord':
-				let guild = client.guilds.cache.get(interaction.guildId);
+				const guild = DiscordTools.getGuild(interaction.guildId);
 				/* TODO: Wait for all text channels to be created before continue */
 				require('../discordTools/SetupGuildChannels')(client, guild);
 				client.log('Waiting 5 seconds to make sure all text channels are created in guild: ' +
 					interaction.guildId);
+
+				instance.firstTime = true;
+				client.writeInstanceFile(interaction.guildId, instance);
 
 				setTimeout(() => {
 					client.log(`Creating Settings Menus for guild: ${interaction.guildId}`);
