584d6a42fefe24456c469c592450315c1c57b8cf
Alexander Emanuelsson <alexander.emanuelsson94@gmail.com>
2022-02-12 13:13:27 +0100

    Changed formating for rateLimit message

--- src/discordEvents/rateLimit.js
+++ src/discordEvents/rateLimit.js
@@ -1,6 +1,7 @@
 module.exports = {
     name: 'rateLimit',
-    async execute(client, guild, info) {
-        client.log('ERROR', JSON.stringify(info), 'error');
+    async execute(client, info) {
+        client.log('RATELIMIT', `Timeout: ${info.timeout}, Limit: ${info.limit}, Method: ${info.method}` +
+            `, Path: ${info.path}, Route: ${info.route}, Global: ${info.global}`, 'error');
     },
 }
