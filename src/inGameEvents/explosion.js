f1df17c18f0bb9862124793f21f68113e959c141
Alexander Emanuelsson <alexander.emanuelsson94@gmail.com>
2022-03-06 16:03:46 +0100

    If bradleyAPC gets destroyed outside launch, give grid pos

--- src/inGameEvents/explosion.js
+++ src/inGameEvents/explosion.js
@@ -4,6 +4,7 @@ const RustPlusTypes = require('../util/rustplusTypes.js');
 const Timer = require('../util/timer');
 
 const PATROL_HELI_DOWNED_RADIUS = 400;
+const LAUNCH_SITE_RADIUS = 250;
 
 module.exports = {
     checkEvent: function (rustplus, client, info, mapMarkers, teamInfo, time) {
@@ -56,9 +57,12 @@ module.exports = {
                     }
                     else {
                         /* Bradley APC just got destroyed */
+                        let atLaunch = module.exports.isBradleyExplosionAtLaunchSite(marker.x, marker.y, rustplus);
+                        pos = (atLaunch) ? 'Launch Site' : pos;
+
                         rustplus.sendEvent(
                             rustplus.notificationSettings.bradleyApcDestroyed,
-                            'Bradley APC was destroyed at Launch Site.');
+                            `Bradley APC was destroyed at ${pos}.`);
 
                         if (!rustplus.firstPoll) {
                             rustplus.bradleyRespawnTimers[marker.id] = new Timer.timer(
@@ -117,5 +121,15 @@ module.exports = {
             args[0].bradleyRespawnTimers[parseInt(args[1])].stop();
             delete args[0].bradleyRespawnTimers[parseInt(args[1])];
         }
-    }
+    },
+
+    isBradleyExplosionAtLaunchSite: function (x, y, rustplus) {
+        /* Check where the explosion marker is located, if near Launch Site, return true */
+        for (let monument of rustplus.mapMonuments) {
+            if (monument.token === 'launchsite') {
+                return (Map.getDistance(x, y, monument.x, monument.y) <= LAUNCH_SITE_RADIUS);
+            }
+        }
+        return false;
+    },
 }
