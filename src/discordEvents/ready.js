d2e15c4a1f2e222b55a4d15558df147aaaffa101
Alexander Emanuelsson <alexander.emanuelsson94@gmail.com>
2022-03-18 17:18:17 +0100

    Force username, nickname and avatar for bot

--- src/discordEvents/ready.js
+++ src/discordEvents/ready.js
@@ -2,10 +2,13 @@ module.exports = {
     name: 'ready',
     once: true,
     async execute(client) {
-        client.user.setActivity('/help', { type: 'LISTENING' });
         client.log('INFO', 'LOGGED IN AS: ' + client.user.tag);
+        client.user.setUsername('rustPlusPlus');
+        client.user.setAvatar('./src/images/rustplusplus_logo.png');
+        client.user.setActivity('/help', { type: 'LISTENING' });
 
         client.guilds.cache.forEach(async (guild) => {
+            guild.me.setNickname('rustPlusPlus');
             await client.setupGuild(guild);
         });
 
