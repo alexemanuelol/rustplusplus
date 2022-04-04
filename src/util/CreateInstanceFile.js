7a936715f19c302f07735f8f1f1dfeb882294103
Alexander Emanuelsson <alexander.emanuelsson94@gmail.com>
2022-04-04 18:38:37 +0200

    Automatically add missing properties in instance file

--- src/util/CreateInstanceFile.js
+++ src/util/CreateInstanceFile.js
@@ -33,4 +33,81 @@ module.exports = (client, guild) => {
             serverList: {}
         }, null, 2));
     }
+    else {
+        let inst = client.readInstanceFile(guild.id);
+
+        if (!inst.hasOwnProperty('firstTime'))
+            inst.firstTime = true;
+
+        if (!inst.hasOwnProperty('generalSettings')) {
+            inst.generalSettings = client.readGeneralSettingsTemplate();
+        }
+        else {
+            const generalSettings = client.readGeneralSettingsTemplate();
+
+            for (const [key, value] of Object.entries(generalSettings)) {
+                if (!inst.generalSettings.hasOwnProperty(key)) {
+                    inst.generalSettings[key] = value;
+                }
+            }
+        }
+
+        if (!inst.hasOwnProperty('notificationSettings')) {
+            inst.notificationSettings = client.readNotificationSettingsTemplate();
+        }
+        else {
+            const notificationSettings = client.readNotificationSettingsTemplate();
+
+            for (const [key, value] of Object.entries(notificationSettings)) {
+                if (!inst.notificationSettings.hasOwnProperty(key)) {
+                    inst.notificationSettings[key] = value;
+                }
+            }
+        }
+
+        if (!inst.hasOwnProperty('channelId')) {
+            inst.channelId = {
+                category: null,
+                information: null,
+                servers: null,
+                settings: null,
+                events: null,
+                teamchat: null,
+                switches: null,
+                alarms: null,
+                activity: null
+            }
+        }
+        else {
+            if (!inst.channelId.hasOwnProperty('category')) inst.channelId.category = null;
+            if (!inst.channelId.hasOwnProperty('information')) inst.channelId.information = null;
+            if (!inst.channelId.hasOwnProperty('servers')) inst.channelId.servers = null;
+            if (!inst.channelId.hasOwnProperty('settings')) inst.channelId.settings = null;
+            if (!inst.channelId.hasOwnProperty('events')) inst.channelId.events = null;
+            if (!inst.channelId.hasOwnProperty('teamchat')) inst.channelId.teamchat = null;
+            if (!inst.channelId.hasOwnProperty('switches')) inst.channelId.switches = null;
+            if (!inst.channelId.hasOwnProperty('alarms')) inst.channelId.alarms = null;
+            if (!inst.channelId.hasOwnProperty('activity')) inst.channelId.activity = null;
+        }
+
+        if (!inst.hasOwnProperty('informationMessageId')) {
+            inst.informationMessageId = {
+                server: null,
+                event: null,
+                team: null
+            }
+        }
+        else {
+            if (!inst.informationMessageId.hasOwnProperty('server')) inst.informationMessageId.server = null;
+            if (!inst.informationMessageId.hasOwnProperty('event')) inst.informationMessageId.event = null;
+            if (!inst.informationMessageId.hasOwnProperty('team')) inst.informationMessageId.team = null;
+        }
+
+        if (!inst.hasOwnProperty('switches')) inst.switches = {};
+        if (!inst.hasOwnProperty('alarms')) inst.alarms = {};
+        if (!inst.hasOwnProperty('markers')) inst.markers = {};
+        if (!inst.hasOwnProperty('serverList')) inst.serverList = {};
+
+        client.writeInstanceFile(guild.id, inst);
+    }
 };
