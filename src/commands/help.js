685decd7fba46a14869bbf47603fae7ce85fc366
Alexander Emanuelsson <alexander.emanuelsson94@gmail.com>
2022-05-09 16:29:50 +0200

    Update help command

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,37 +1,30 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const DiscordTools = require('../discordTools/discordTools');
-const { MessageEmbed } = require('discord.js');
+const { MessageButton, MessageActionRow } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('help')
 		.setDescription('Display help message'),
 	async execute(client, interaction) {
-		let instance = client.readInstanceFile(interaction.guildId);
-
-		if (instance.role !== null) {
-			if (!interaction.member.permissions.has('ADMINISTRATOR') &&
-				!interaction.member.roles.cache.has(instance.role)) {
-				let role = DiscordTools.getRole(interaction.guildId, instance.role);
-				let str = `You are not part of the '${role.name}' role, therefore you can't run bot commands.`;
-				await client.interactionReply(interaction, {
-					embeds: [new MessageEmbed()
-						.setColor('#ff0040')
-						.setDescription(`\`\`\`diff\n- ${str}\n\`\`\``)],
-					ephemeral: true
-				});
-				client.log('WARNING', str);
-				return;
-			}
-		}
-
-		let str = `TBD.`;
 		await client.interactionReply(interaction, {
-			embeds: [new MessageEmbed()
-				.setColor('#ce412b')
-				.setDescription(`\`\`\`diff\n- ${str}\n\`\`\``)],
+			components: [new MessageActionRow()
+				.addComponents(
+					new MessageButton()
+						.setStyle('LINK')
+						.setLabel('DEVELOPER')
+						.setURL('https://github.com/alexemanuelol'),
+					new MessageButton()
+						.setStyle('LINK')
+						.setLabel('REPOSITORY')
+						.setURL('https://github.com/alexemanuelol/rustPlusPlus'),
+					new MessageButton()
+						.setStyle('LINK')
+						.setLabel('DOCUMENTATION')
+						.setURL('https://github.com/alexemanuelol/rustPlusPlus/blob/master/docs/documentation.md')
+				)
+			],
 			ephemeral: true
 		});
-		client.log('WARNING', str);
+		client.log('INFO', 'Displaying help message.');
 	},
 };
