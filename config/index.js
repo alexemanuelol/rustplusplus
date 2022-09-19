9c9323bd94f8a97ee5a42091c71d095e5b9e6aef
Matt <psykzz@users.noreply.github.com>
2022-09-19 20:23:30 +0100

    update username

--- config/index.js
+++ config/index.js
@@ -4,7 +4,7 @@ module.exports = {
         showCallStackError: process.env.LOG_CALL_STACK || false
     },
     discord: {
-        username: process.env.DISCORD_USERNAME || 'RustPlus',
+        username: process.env.DISCORD_USERNAME || 'rustPlusPlus',
         clientId: process.env.DISCORD_CLIENT_ID || "",
         token: process.env.DISCORD_TOKEN || ""
     }
