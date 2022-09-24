ac1f0168b39b6dd6a1eadec92221e61be2f78132
Matt Smith <matt.daemon660@gmail.com>
2022-09-25 00:29:35 +0100

    add rpp prefix

--- config/index.js
+++ config/index.js
@@ -1,11 +1,11 @@
 module.exports = {
     general: {
-        pollingIntervalMs: process.env.POLLING_INTERVAL || 10000,
-        showCallStackError: process.env.LOG_CALL_STACK || false
+        pollingIntervalMs: process.env.RPP_POLLING_INTERVAL || 10000,
+        showCallStackError: process.env.RPP_LOG_CALL_STACK || false
     },
     discord: {
-        username: process.env.DISCORD_USERNAME || 'rustPlusPlus',
-        clientId: process.env.DISCORD_CLIENT_ID || "",
-        token: process.env.DISCORD_TOKEN || ""
+        username: process.env.RPP_DISCORD_USERNAME || 'rustPlusPlus',
+        clientId: process.env.RPP_DISCORD_CLIENT_ID || "",
+        token: process.env.RPP_DISCORD_TOKEN || ""
     }
 };
