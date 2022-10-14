2a10730884ce58bb303b959fb290bce29c052c44
Alexander Emanuelsson <alexander.emanuelsson94@gmail.com>
2022-10-14 23:09:45 +0200

    Add russian language file by MsIGROC#8655

--- src/util/external/missingTranslation.js
+++ src/util/external/missingTranslation.js
@@ -13,7 +13,6 @@ const directoryPath = '../../languages/';
 const enTranslation = JSON.parse(Fs.readFileSync(directoryPath + 'en.json'));
 const compTranslation = JSON.parse(Fs.readFileSync(directoryPath + compLanguage));
 
-
 console.log(`en.json in ${compLanguage}:`);
 for (const item in enTranslation) {
     if (!(item in compTranslation)) console.log(`  '${item}' missing in language file ${compLanguage}`);
@@ -22,4 +21,20 @@ for (const item in enTranslation) {
 console.log(`\n${compLanguage} in en.json:`);
 for (const item in compTranslation) {
     if (!(item in enTranslation)) console.log(`'${item}' missing in language file en.json`);
+}
+
+const enKeys = Object.keys(enTranslation).length;
+const compKeys = Object.keys(compTranslation).length;
+console.log(`en.json keys: ${enKeys}\n${compLanguage} keys: ${compKeys}`)
+
+if (enKeys === compKeys) {
+    const enArray = Object.keys(enTranslation);
+    const compArray = Object.keys(compTranslation);
+
+    for (let i = 0; i < enKeys; i++) {
+        if (enArray[i] !== compArray[i]) {
+            console.log('Not in the same order.')
+            break;
+        }
+    }
 }
