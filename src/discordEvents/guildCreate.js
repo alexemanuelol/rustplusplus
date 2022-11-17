8abcbf0c2ec2ce3713920db096bbe5f56d3b33f6
Alexander Emanuelsson <alexander.emanuelsson94@gmail.com>
2022-11-17 23:30:36 +0100

    Fix fail to add essentials at guildCreate

--- src/discordEvents/guildCreate.js
+++ src/discordEvents/guildCreate.js
@@ -21,6 +21,12 @@
 module.exports = {
     name: 'guildCreate',
     async execute(client, guild) {
+        require('../util/CreateInstanceFile')(client, guild);
+        require('../util/CreateCredentialsFile')(client, guild);
+        client.fcmListenersLite[guild.id] = new Object();
+
+        client.loadGuildIntl(guild.id);
+
         await client.setupGuild(guild);
     },
 }
