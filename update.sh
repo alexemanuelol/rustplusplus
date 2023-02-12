27f146dc062c8a505654dfdd5f31aed1bb8ccbfd
Pandeo <theo.nard18@gmail.com>
2023-02-12 12:36:34 +0100

    Fix update.sh exit code check
    Check exit code directly with if ! mycmd, not indirectly with $?
    
    https://github.com/PandeoF1/rustPlusPlus/issues/13

--- update.sh
+++ update.sh
@@ -19,26 +19,22 @@
 #   https://github.com/alexemanuelol/rustPlusPlus
 #
 
-git stash
-if [ $? -ne 0 ]; then
+if ! git stash; then
     echo "Could not successfully stash current changes"
     exit 1
 fi
 
-git pull --rebase origin master
-if [ $? -ne 0 ]; then
+if ! git pull --rebase origin master; then
     echo "Could not successfully pull the latest changes to your local repository"
     exit 1
 fi
 
-git stash pop
-if [ $? -ne 0 ]; then
+if ! git stash pop; then
     echo "Could not successfully pop the stash"
     exit 1
 fi
 
-npm install
-if [ $? -ne 0 ]; then
+if ! npm install; then
     echo "Could not successfully install packages"
     exit 1
 fi
