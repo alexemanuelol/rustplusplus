4db101dd3b8d5db74fe3d54cee9a70dc4dbe830b
Obama <39924544+ObamaCareLover@users.noreply.github.com>
2023-04-11 21:54:57 -0500

    Review edits

--- src/rustplusEvents/message.js
+++ src/rustplusEvents/message.js
@@ -112,7 +112,8 @@ async function messageBroadcastCameraRays(rustplus, client, message) {
 
 		let str = `${client.intlGet(rustplus.guildId, 'scannedCameras', { num: rustplus.scannedCameras })}, `;
 		
-		str += rustplus.cameraPlayerNames.length != 0 ? `${client.intlGet(rustplus.guildId, 'players')}: ${rustplus.cameraPlayerNames.join(', ')}` : `${client.intlGet(rustplus.guildId, 'noPlayersFound')}`;
+		str += rustplus.cameraPlayerNames.length !== 0 ? `${client.intlGet(rustplus.guildId, 'players')}: 
+            ${rustplus.cameraPlayerNames.join(', ')}` : `${client.intlGet(rustplus.guildId, 'noPlayersFound')}`;
         
         if (!rustplus.isCamCommandInGame) {
             await DiscordMessages.sendDiscordCommandResponseMessage(rustplus, client, rustplus.camCommandMessage, str);
