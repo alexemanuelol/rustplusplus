fd0f4417d9159a699bd427e4589c24c33b68f5f5
Rene Renken <r.p.renken@web.de>
2023-08-01 23:35:46 +0200

    solving https://github.com/alexemanuelol/rustplusplus/pull/204#discussion_r1281052500

--- src/commands/voice.js
+++ src/commands/voice.js
@@ -1,6 +1,6 @@
 /*
     Copyright (C) 2023 Alexander Emanuelsson (alexemanuelol)
-    Copyright (C) 2023 Squidysquid1
+    Copyright (C) 2023 FaiThiX
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
