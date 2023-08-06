08aa5890e4a649258b46f079837a50209c047ce8
Matt <psykzz@users.noreply.github.com>
2023-08-06 13:36:18 +0100

    fix: correct arguments for discord event "error" (#222)
    guild is never actually passed to the error, nor was it used.

--- src/discordEvents/error.js
+++ src/discordEvents/error.js
@@ -20,7 +20,7 @@
 
 module.exports = {
     name: 'error',
-    async execute(client, guild, error) {
+    async execute(client, error) {
         client.log(client.intlGet(null, 'errorCap'), error, 'error');
         process.exit(1);
     },
