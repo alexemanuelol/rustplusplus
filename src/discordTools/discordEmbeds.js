6477921dfd5ace7054efeb4f4c74b08dfe5db5de
MrFiNka <48646488+MrFiNka@users.noreply.github.com>
2024-07-22 22:24:51 +0300

    Update discordEmbeds.js

--- src/discordTools/discordEmbeds.js
+++ src/discordTools/discordEmbeds.js
@@ -1074,7 +1074,7 @@ module.exports = {
     },
 
     getHelpEmbed: function (guildId) {
-        const repository = 'https://github.com/alexemanuelol/rustplusplus';
+        const repository = 'https://github.com/MrFiNka/rustplusplus';
         const credentials = `${repository}/blob/master/docs/credentials.md`;
         const pairServer = `${repository}/blob/master/docs/pair_and_connect_to_server.md`;
         const commands = `${repository}/blob/master/docs/commands.md`;
