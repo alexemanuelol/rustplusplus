f09fdda1440d22e1527a04e706d0e55f8929ecbb
dextermyles <dexter.brock@gmail.com>
2025-04-23 07:35:43 -0400

    config

--- config/index.js
+++ config/index.js
@@ -18,19 +18,17 @@
 
 */
 
-const discord_temp = `MzQ5MTI5MDEzMTY1MjkzNTcx.G5CKZt.rEF5T5HjpPKN_nTKtMu62Qk-WLuWR2-DMrTXWg`;
-
 module.exports = {
     general: {
         language: process.env.RPP_LANGUAGE || 'en',
-        pollingIntervalMs: process.env.RPP_POLLING_INTERVAL || 7500,
-        showCallStackError: true,
+        pollingIntervalMs: process.env.RPP_POLLING_INTERVAL || 10000,
+        showCallStackError: false,
         reconnectIntervalMs: process.env.RPP_RECONNECT_INTERVAL || 15000,
     },
     discord: {
         username: process.env.RPP_DISCORD_USERNAME || 'BigRaidHunter',
         clientId: process.env.RPP_DISCORD_CLIENT_ID || '349129013165293571',
-        token: process.env.RPP_DISCORD_TOKEN || discord_temp,
+        token: process.env.RPP_DISCORD_TOKEN,
         needAdminPrivileges: process.env.RPP_NEED_ADMIN_PRIVILEGES || true, /* If true, only admins can delete (server, switch..), manage credentials and reset a channel */
     }
 };
