0e917d5eff969c610098884c57ea812c0e772f17
Alexander Emanuelsson <alexander.emanuelsson94@gmail.com>
2026-05-15 12:45:01 +0200

    Add afk prefix command

---
+++ src/prefixCommands/afk.ts
@@ -0,0 +1,59 @@
+/*
+    Copyright (C) 2026 Alexander Emanuelsson (alexemanuelol)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    https://github.com/alexemanuelol/rustplusplus
+
+*/
+
+import * as rp from 'rustplus-ts';
+import * as discordjs from 'discord.js';
+
+import { log, guildInstanceManager as gim, localeManager as lm } from '../../index';
+import { RustPlusInstance } from "../managers/rustPlusManager";
+import { GuildInstance } from '../managers/guildInstanceManager';
+
+export const name = 'afk';
+
+export async function execute(rpInstance: RustPlusInstance, args: string[],
+    message: rp.AppTeamMessage | discordjs.Message):
+    Promise<boolean> {
+    const fn = `[prefixCommand: ${name}]`;
+    const logParam = {
+        guildId: rpInstance.guildId,
+        serverId: rpInstance.serverId,
+        serverName: rpInstance.serverName
+    };
+
+    const inGame = Object.hasOwn(message, 'steamId') ? true : false;
+    const guildId = rpInstance.guildId;
+    const gInstance = gim.getGuildInstance(guildId) as GuildInstance;
+    const language = gInstance.generalSettings.language;
+
+    if (rpInstance.rpTeamInfo === null) return false;
+
+    let response = '';
+    for (const member of rpInstance.rpTeamInfo.members.values()) {
+        if (member.isAfk()) {
+            response += `${member.appTeamInfoMember.name} (${member.getAfkTime('dhs')}), `;
+        }
+    }
+
+    response = response !== '' ? `${response.slice(0, -2)}` : lm.getIntl(language, 'noOneIsAfk');
+    rpInstance.sendPrefixCommandResponse(response, inGame);
+    log.info(`${fn} ${response}`, logParam);
+
+    return true;
+}
