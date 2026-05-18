91beb2a9e0aaab0b0098d9fc8bef7c8d3380a3d3
Alexander Emanuelsson <alexander.emanuelsson94@gmail.com>
2026-05-18 21:55:18 +0200

    Add offline/online prefix commands

---
+++ src/prefixCommands/online.ts
@@ -0,0 +1,60 @@
+/*
+    Copyright (C) 2026 Alexander Emanuelsson (alexemanuelol)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    https://github.com/alexemanuelol/rustplusplus
+
+*/
+
+import * as rp from 'rustplus-ts';
+import * as discordjs from 'discord.js';
+
+import { log, guildInstanceManager as gim, localeManager as lm } from '../../index';
+import { RustPlusInstance } from "../managers/rustPlusManager";
+import { GuildInstance } from '../managers/guildInstanceManager';
+
+export const name = 'online';
+
+export async function execute(rpInstance: RustPlusInstance, args: string[],
+    message: rp.AppTeamMessage | discordjs.Message):
+    Promise<boolean> {
+    const fn = `[prefixCommand: ${name}]`;
+    const logParam = {
+        guildId: rpInstance.guildId,
+        serverId: rpInstance.serverId,
+        serverName: rpInstance.serverName
+    };
+
+    const inGame = Object.hasOwn(message, 'steamId') ? true : false;
+    const guildId = rpInstance.guildId;
+    const gInstance = gim.getGuildInstance(guildId) as GuildInstance;
+    const language = gInstance.generalSettings.language;
+
+    if (rpInstance.rpTeamInfo === null) return false;
+
+    const members: string[] = [];
+    for (const member of rpInstance.rpTeamInfo.members.values()) {
+        if (member.appTeamInfoMember.isOnline) {
+            members.push(member.appTeamInfoMember.name);
+        }
+    }
+    const str = members.length === 0 ? lm.getIntl(language, 'noOneIsOnline') :
+        `(${members.length}/${rpInstance.rpTeamInfo.members.size}) ${members.join(', ')}`;
+
+    rpInstance.sendPrefixCommandResponse(str, inGame);
+    log.info(`${fn} ${str}`, logParam);
+
+    return true;
+}
